9ab40caaaf028c0329f31101d8cb156a8050aafa
capi <cpinelly@gmail.com>
2017-12-16 01:14:18 -0400

    Agregada posibilidad de configurar tasa de USD/VEF.

--- app/src/main/java/com/capicp/lifeexpensecalculator/MainActivity.kt
+++ app/src/main/java/com/capicp/lifeexpensecalculator/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.capicp.lifeexpensecalculator
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     val workHourRate:Float = 5.0f
-    val usdVefRate:Float = 32000f
+    var usdVefRate:Int = 87000
 
     lateinit var vefExpenseET:EditText
     lateinit var usdExpenseTV:TextView
@@ -28,18 +32,7 @@ class MainActivity : AppCompatActivity() {
         vefExpenseET.addTextChangedListener(object : TextWatcher{
             override fun afterTextChanged(edit: Editable?) {
 
-                if ( edit != null && edit.length > 0){
-                    val vefValue:Float = edit.toString().toFloat()
-
-                    val usdValue = vefValue / usdVefRate
-
-                    val hoursValue = usdValue/workHourRate
-
-                    usdExpenseTV.setText("$${"%.2f".format(usdValue)}")
-                    lifeExpenseTV.setText("${"%.1f".format(hoursValue)}")
-
-
-                }
+                updateCalculations()
 
             }
 
@@ -52,7 +45,48 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-      }
+    }
+
+    fun updateCalculations(){
+
+        val edit:Editable = vefExpenseET.text
+        if ( edit != null && edit.length > 0){
+            val vefValue:Float = edit.toString().toFloat()
+
+            val usdValue = vefValue / usdVefRate
+
+            val hoursValue = usdValue/workHourRate
+
+            usdExpenseTV.setText("$${"%.2f".format(usdValue)}")
+            lifeExpenseTV.setText("${"%.1f".format(hoursValue)}")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val sp = getSharedPreferences(getString(R.string.sp_file_settings), Context.MODE_PRIVATE)
+        usdVefRate = sp.getInt(getString(R.string.sp_usd_vef_rate), 80000)
+
+        updateCalculations()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.settings -> {
+                val int = Intent(this, SettingsActivity::class.java)
+                startActivity(int)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
 
 
